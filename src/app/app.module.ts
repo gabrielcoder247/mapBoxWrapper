22b410ddc3f67f14b4c0c2aa04efb6a2d6141919
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-07-08 14:23:52 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
