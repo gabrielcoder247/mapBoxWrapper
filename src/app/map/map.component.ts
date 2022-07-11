18f26dff4db9a79e80f4da885e47a0c06c5a5c11
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-07-11 18:01:14 +0100

    Install ngrx

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
-import { Map } from 'maplibre-gl';
+import { Map, NavigationControl, Marker } from 'maplibre-gl';
 
 @Component({
   selector: 'app-map',
@@ -27,6 +27,15 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
       zoom: initialState.zoom
     });
 
+    let controlOption = {
+      showCompass: true,
+      showZoom: true,
+      visualizePitch: false,
+    }
+    this.map.addControl(new NavigationControl(controlOption), 'top-right');
+    new Marker({color: "#FF0000"})
+      .setLngLat([139.7525,35.6846])
+      .addTo(this.map);
   }
 
   ngOnDestroy() {
